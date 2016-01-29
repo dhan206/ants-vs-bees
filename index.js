d808f801b381908f5e7cf4a54fc732971f556f8d
DereK Han <derekhan206@gmail.com>
2016-01-29 15:07:36 -0800

    Add food ant, grower ant, wall ant, hungry ant, fire ant, water, and scuba ant

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ var ants = require('./lib/ants_ui.js'); //load the game (UI-level) to interact w
 
 if(process.argv[2] === '--debug'){
   //the scenario to play
-  var colony = ants.AntColony.createDefaultColony();
+  var colony = ants.AntColony.createWetColony();
   var hive = ants.Hive.createTestHive();
   var game = new ants.AntGame(colony, hive);
   
   //run hard-coded commands
-  game.deployAnt('Grower', '0,0'); //for example
-  game.deployAnt('Thrower', '0,1');
-  game.takeTurn(); //for example
+  game.deployAnt('Fire', '0,4'); //for example
   game.takeTurn();
   game.takeTurn();
-  ants.showMapOf(game); //show the board, for example
+  game.takeTurn();
+  game.takeTurn();
+//   ants.showMapOf(game); //show the board, for example
 
 
-  //ants.play(game); //launch the interactive version from here
+  ants.play(game); //launch the interactive version from here
 
 }
 else {
   //initialize the game to play (not interactively selected yet)
-  var colony = ants.AntColony.createDefaultColony();
+  var colony = ants.AntColony.createTestColony();
   var hive = ants.Hive.createTestHive();
   var game = new ants.AntGame(colony, hive);
 
